e514ff2ca24d330f99629ed63b235e8b3bb929b8
Murray Williams <murray.williams@ventureweb.com>
2018-04-30 18:19:25 +0200

    added GA tracking to site + minor content updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Murray Williams - Freelance UX Designer & Developer',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-118410471-1",
+        head: true,
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
 
